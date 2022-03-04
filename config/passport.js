57947092383e667c628856c29a2e7980b86ee759
Sebastian Deymonnaz <adeymonn@hotmail.com>
2022-03-03 23:14:21 -0300

    hash done

--- config/passport.js
+++ config/passport.js
@@ -3,38 +3,51 @@ import { Strategy as LocalStrategy } from "passport-local";
 import bcrypt from "bcrypt";
 import User from "../models/usuario.js";
 import {carritoDao as newCart } from '../Daos/DAOs.js';
-//import { SendEmail } from '../notifications/email.js';
-//const sendEmailToAdmin = new SendEmail();
-//import upload from '../multer.js';
+import upload from '../utils/uploadFiles.js';
+import { SendEmail } from '../utils/sendEmail.js';
+const sendEmailToAdmin = new SendEmail();
 
 
 //Encripcion de contraseña ////////////////////////////////////////////////////////////////////////////////////////////////
 //Funcion para encriptar contraseña
-/* const hashPassword = (password) => {
+const hashPassword = (password) => {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 };
 
 //Funcion para desencriptar contraseña
 const isValidPassword = (user, password) => {
     return bcrypt.compareSync(password, user.password);
-}; */
+};
 
 //Estrategia de passport-local para autenticacion de usuarios
 //Sign-in de usuarios existentes //////////////////////////////////////////////////////////////////////////////////////////////
 passport.use("local-login", new LocalStrategy(async (username, password, done) => {
     let user = await User.findOne({
         username: username,
-        password: password
     });
 
     console.log('user encontrado en MongoDB:', user);
 
-    if (user) {
-        done(null, user);
-        return;
+    if(!user) {
+        return done(null, false, { message: "Usuario no existente" });
     }
 
-    done(null, user);
+    if (!isValidPassword(user, password)) {
+        return done(null, false, { message: "Usuario o contraseña incorrectos" });
+    }
+
+    return done(null, 
+        {
+            username: user.username,
+            nombre: user.nombre,
+            direccion: user.direccion,
+            edad: user.edad,
+            telefono: user.telefono,
+            foto: user.foto,
+            cart: user.cart
+        },    
+        { message: "Login exitoso" }
+    );
 }));
 
 //Sign-up de usuarios nuevos /////////////////////////////////////////////////////////////////////////////////////////////////
@@ -55,20 +68,29 @@ passport.use("local-signup", new LocalStrategy(
     if (!user) {
         let userNew = await User({
             username,
-            password,
+            password: hashPassword(password),
             nombre: req.body.nombre,
             direccion: req.body.direccion,
             edad: req.body.edad,
             telefono: req.body.telefono,
-            foto: req.body.foto,
             cart: await newCart.saveCart()
-        })
-        await userNew.save({returnNewDocument: true});
-        return done(null, userNew);
-        }
-        return done(null, false, { message: "Usuario ya existe" });
+        });
+        upload.upload(req, res, async (err) => {
+            if (err) {
+                console.log('error:', err);
+                return res.send(err);
+            }
+            userNew.foto = req.file.filename;
+            await userNew.save({returnNewDocument: true});
+            console.log('userNew guardado en MongoDB:', userNew);
+            done(null, userNew);
+        });
+        sendEmailToAdmin.sendEmail(process.env.ADMIN_EMAIL, "Nuevo registro", `El usuario ${userNew.username} se ha registrado con estos detalles: \n Nombre: ${userNew.nombre} \n Dirección: ${userNew.direccion} \n Edad: ${userNew.edad} \n Telefono: ${userNew.telefono}`);
+    } else {
+        done(null, false, user, { message: "Usuario ya existe" });
     }
-));
+}));
+
 
 // Serializacion //////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Serializacion de usuarios
---
+++ utils/sendEmail.js
@@ -0,0 +1,33 @@
+import { createTransport } from "nodemailer";
+
+export class SendEmail {
+  constructor() {
+    this.transporter = createTransport({
+      service: "gmail",
+      port: 587,
+      auth: {
+        user: process.env.ADMIN_EMAIL,
+        pass: "nopvoshqjfduqfbm"
+      }
+    });
+  }
+
+    sendEmail(email, subject, text) {
+        return new Promise((resolve, reject) => {
+            this.transporter.sendMail({
+                from: "Server Proyecto Backend",
+                to: email,
+                subject: subject,
+                text: text
+            }
+            , (err, info) => {
+                console.log(err);
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(info);
+                }
+            });
+        });
+    }
+}
